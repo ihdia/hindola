1fcf0a5de656658eb44ff6739d0cd4c021f2dc9e
Aaron Augustine <asajadidas@gmail.com>
2019-09-21 04:21:52 +0530

    HInDoLA Files Added

---
+++ save/dump.sql
@@ -0,0 +1,328 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: annotation_web
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bookcompletion`
+--
+
+DROP TABLE IF EXISTS `bookcompletion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `bookcompletion` (
+  `name` varchar(200) DEFAULT NULL,
+  `total_pages` int(7) DEFAULT NULL,
+  `completed_pages` int(7) DEFAULT NULL,
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookcompletion`
+--
+
+LOCK TABLES `bookcompletion` WRITE;
+/*!40000 ALTER TABLE `bookcompletion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookcompletion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookmarks`
+--
+
+DROP TABLE IF EXISTS `bookmarks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `bookmarks` (
+  `file` varchar(200) DEFAULT NULL,
+  UNIQUE KEY `file` (`file`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookmarks`
+--
+
+LOCK TABLES `bookmarks` WRITE;
+/*!40000 ALTER TABLE `bookmarks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookmarks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `corrections`
+--
+
+DROP TABLE IF EXISTS `corrections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `corrections` (
+  `pfile` varchar(250) DEFAULT NULL,
+  `pfilepath` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `pfile` (`pfile`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `corrections`
+--
+
+LOCK TABLES `corrections` WRITE;
+/*!40000 ALTER TABLE `corrections` DISABLE KEYS */;
+/*!40000 ALTER TABLE `corrections` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `imagelinks`
+--
+
+DROP TABLE IF EXISTS `imagelinks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `imagelinks` (
+  `file` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `links` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`file`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagelinks`
+--
+
+LOCK TABLES `imagelinks` WRITE;
+/*!40000 ALTER TABLE `imagelinks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `imagelinks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `info`
+--
+
+DROP TABLE IF EXISTS `info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `info` (
+  `username` varchar(255) DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `localurl` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info`
+--
+
+LOCK TABLES `info` WRITE;
+/*!40000 ALTER TABLE `info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `json_v1`
+--
+
+DROP TABLE IF EXISTS `json_v1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `json_v1` (
+  `filename` varchar(200) DEFAULT NULL,
+  `json_data` text,
+  `fileurl` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `json_v1`
+--
+
+LOCK TABLES `json_v1` WRITE;
+/*!40000 ALTER TABLE `json_v1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `json_v1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `json_v2`
+--
+
+DROP TABLE IF EXISTS `json_v2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `json_v2` (
+  `filename` varchar(200) DEFAULT NULL,
+  `json_data` text,
+  `fileurl` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `json_v2`
+--
+
+LOCK TABLES `json_v2` WRITE;
+/*!40000 ALTER TABLE `json_v2` DISABLE KEYS */;
+/*!40000 ALTER TABLE `json_v2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagerequests`
+--
+
+DROP TABLE IF EXISTS `pagerequests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `pagerequests` (
+  `name` varchar(20) DEFAULT NULL,
+  `book` varchar(40) DEFAULT NULL,
+  `start_page` int(10) DEFAULT NULL,
+  `current_page` int(10) DEFAULT NULL,
+  `end_page` int(10) DEFAULT NULL,
+  `skipped` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagerequests`
+--
+
+LOCK TABLES `pagerequests` WRITE;
+/*!40000 ALTER TABLE `pagerequests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagerequests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `puids`
+--
+
+DROP TABLE IF EXISTS `puids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `puids` (
+  `pfile` varchar(255) DEFAULT NULL,
+  `pfilepath` varchar(255) DEFAULT NULL,
+  `saved_date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `puids`
+--
+
+LOCK TABLES `puids` WRITE;
+/*!40000 ALTER TABLE `puids` DISABLE KEYS */;
+/*!40000 ALTER TABLE `puids` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ruids`
+--
+
+DROP TABLE IF EXISTS `ruids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `ruids` (
+  `ruid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ruids`
+--
+
+LOCK TABLES `ruids` WRITE;
+/*!40000 ALTER TABLE `ruids` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ruids` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uids`
+--
+
+DROP TABLE IF EXISTS `uids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `uids` (
+  `ufile` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uids`
+--
+
+LOCK TABLES `uids` WRITE;
+/*!40000 ALTER TABLE `uids` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uids` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `viewer`
+--
+
+DROP TABLE IF EXISTS `viewer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `viewer` (
+  `Website` char(100) DEFAULT NULL,
+  `Link` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `viewer`
+--
+
+LOCK TABLES `viewer` WRITE;
+/*!40000 ALTER TABLE `viewer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `viewer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-15 19:59:28
